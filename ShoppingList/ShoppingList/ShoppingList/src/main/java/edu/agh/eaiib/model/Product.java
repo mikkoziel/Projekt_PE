21c31a908c11270e4dfcb70f9e487ba7cb87155b
Monika Koltunik <monika.koltunik@tesco.com>
2020-05-06 22:03:22 +0200

    Remove lombok

--- ShoppingList/ShoppingList/ShoppingList/src/main/java/edu/agh/eaiib/model/Product.java
+++ ShoppingList/ShoppingList/ShoppingList/src/main/java/edu/agh/eaiib/model/Product.java
@@ -1,9 +1,48 @@
 package edu.agh.eaiib.model;
 
-import lombok.Data;
-
-@Data
 public class Product {
     String name;
     int amount;
+
+    public Product() {
+    }
+
+    public Product(String name, int amount) {
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        if (amount != product.amount) return false;
+        return name != null ? name.equals(product.name) : product.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + amount;
+        return result;
+    }
 }
