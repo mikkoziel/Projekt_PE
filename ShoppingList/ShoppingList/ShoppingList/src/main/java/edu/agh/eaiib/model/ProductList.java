9270a6c4abdac848cae4e99b8bbea0c5b04d2bfd
Dwoosh <34167272+Dwoosh@users.noreply.github.com>
2020-05-07 12:22:11 +0200

    Add creator and users to product list

--- ShoppingList/ShoppingList/ShoppingList/src/main/java/edu/agh/eaiib/model/ProductList.java
+++ ShoppingList/ShoppingList/ShoppingList/src/main/java/edu/agh/eaiib/model/ProductList.java
@@ -1,29 +1,42 @@
 package edu.agh.eaiib.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductList {
     private String name;
-    private String username;
+    private String creatorName;
+    private List<String> usersNames;
     private final List<Product> products;
 
-    public ProductList(String name, String username, List<Product> products) {
+    public ProductList(String name, String listCreatorName){
+        this(name, listCreatorName, new ArrayList<Product>(), new ArrayList<String>());
+    }
+
+    public ProductList(String name, String listCreatorName, List<Product> products){
+        this(name, listCreatorName, products, new ArrayList<String>());
+    }
+
+    public ProductList(String name, String listCreatorName, List<Product> products, List<String> users) {
         this.name = name;
-        this.username = username;
+        this.creatorName = listCreatorName;
         this.products = products;
+        this.usersNames = users;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getUsername() {
-        return username;
+    public String getCreatorName() {
+        return creatorName;
     }
 
     public List<Product> getProducts() {
         return products;
     }
+
+    public List<String> getUsers() { return usersNames; }
 
     @Override
     public boolean equals(Object o) {
@@ -33,14 +46,14 @@ public class ProductList {
         ProductList that = (ProductList) o;
 
         if (!name.equals(that.name)) return false;
-        if (!username.equals(that.username)) return false;
+        if (!creatorName.equals(that.creatorName)) return false;
         return products.equals(that.products);
     }
 
     @Override
     public int hashCode() {
         int result = name.hashCode();
-        result = 31 * result + username.hashCode();
+        result = 31 * result + creatorName.hashCode();
         result = 31 * result + products.hashCode();
         return result;
     }
@@ -49,7 +62,7 @@ public class ProductList {
     public String toString() {
         return "ProductList{" +
                 "name='" + name + '\'' +
-                ", username='" + username + '\'' +
+                ", username='" + creatorName + '\'' +
                 ", products=" + products +
                 '}';
     }
