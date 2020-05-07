557e73d980717a7d6b81a62a4df9076712a5ac6d
Julia Zarzycka <36897051+Amezu@users.noreply.github.com>
2020-05-07 18:02:02 +0200

    Make ProductListService singleton

--- ShoppingList/src/main/java/edu/agh/eaiib/service/ProductListService.java
+++ ShoppingList/src/main/java/edu/agh/eaiib/service/ProductListService.java
@@ -1,13 +1,20 @@
 package edu.agh.eaiib.service;
 
 import edu.agh.eaiib.model.ProductList;
+import edu.agh.eaiib.repository.GsonProductListRepository;
 import edu.agh.eaiib.repository.ProductListRepository;
 
 public class ProductListService {
-    ProductListRepository productListRepository;
 
-    public ProductListService(ProductListRepository productListRepository) {
-        this.productListRepository = productListRepository;
+    private static final ProductListService instance = new ProductListService();
+    private ProductListRepository productListRepository;
+
+    public static ProductListService getInstance() {
+        return instance;
+    }
+
+    private ProductListService() {
+        this.productListRepository = new GsonProductListRepository("database.json");
     }
 
     public ProductList addProduct() {
